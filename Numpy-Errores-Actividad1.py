637989a6a5d09a4fd23e3c971d38372173974e51
Jose Mejia <jgmejia2@espe.edu.ec>
2021-05-25 10:18:29 -0500

    segundo archivo

--- Numpy-Errores-Actividad1.py
+++ Numpy-Errores-Actividad1.py
@@ -2,9 +2,13 @@
 import numpy as np
 
 #Ingreso valor en cada posicion de la matrix
-a=imput("Inserte un numero en la posicion ")
-b=imput("Inserte un numero en la posicion ")
-c=imput("Inserte un numero en la posicion ")
-d=imput("Inserte un numero en la posicion ")
+a= int(input("Inserte un numero en la posicion [0,0]: "))
+b= int(input("Inserte un numero en la posicion [0,1]: "))
+c= int(input("Inserte un numero en la posicion [1,0]: "))
+d= int(input("Inserte un numero en la posicion [1,1]: "))
 
-M=np.matrix([a, b],[c, d])
+M = np.matrix([[a, b],[c, d]])
+print(M)
+print("La matriz inversa es: ")
+MI= np.linalg.inv(M)
+print(MI)
