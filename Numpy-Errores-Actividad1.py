53c84128ba5edbd70ac9d39243aeaa4fca409f29
Jose Mejia <jgmejia2@espe.edu.ec>
2021-05-24 21:53:30 -0500

    Primera parte codigo

---
+++ Numpy-Errores-Actividad1.py
@@ -0,0 +1,10 @@
+#importacion de la libreria
+import numpy as np
+
+#Ingreso valor en cada posicion de la matrix
+a=imput("Inserte un numero en la posicion ")
+b=imput("Inserte un numero en la posicion ")
+c=imput("Inserte un numero en la posicion ")
+d=imput("Inserte un numero en la posicion ")
+
+M=np.matrix([a, b],[c, d])
