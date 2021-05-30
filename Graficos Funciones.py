1ba08563887a5590afc65b1cb5a593b6d6576f6c
Jose Mejia <jgmejia2@espe.edu.ec>
2021-05-30 14:07:02 -0500

    listo

--- Graficos Funciones.py
+++ Graficos Funciones.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f= lambda x: np.sin(x)
-g= lambda x: np.cos(x)
+f= lambda x: x**2-x+1
+g= lambda x: 2/(x-1)
 
-x=np.linspace(0,10,100)
+x=np.linspace(-10,10,500)
 
 plt.plot(x,f(x))
 plt.plot(x,g(x))
